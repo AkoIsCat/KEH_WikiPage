d1db7fd50b4e89edf9222bd55833862605d12f62
KimEunHye <gpqjr00@naver.com>
2024-01-26 03:01:58 +0900

    Feat: list의 각 item 컴포넌트 생성

---
+++ src/components/MainPage/ListItem.tsx
@@ -0,0 +1,31 @@
+import styled from 'styled-components';
+import { useNavigate } from 'react-router-dom';
+
+import { ListItemProps } from 'types/types';
+
+const ListItem = ({ id, title, contents }: ListItemProps) => {
+  const navigate = useNavigate();
+
+  return (
+    <Item
+      id={`${id}`}
+      onClick={() => navigate(`/${id}`, { state: { title, contents } })}
+    >
+      {title}
+    </Item>
+  );
+};
+
+export default ListItem;
+
+const Item = styled.div`
+  height: 50px;
+  margin: 0px 0;
+  display: flex;
+  font-size: 20px;
+  align-items: center;
+  padding: 10px;
+  border-radius: 5px;
+  border: 1px solid #cdd2ed;
+  cursor: pointer;
+`;
