a74b806ecd01bf1749b9e6085add5420512027d9
KimEunHye <gpqjr00@naver.com>
2024-01-26 02:29:45 +0900

    Feat: Header 내용 추가

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,5 +1,23 @@
+import styled from 'styled-components';
+
 const Header = () => {
-  return;
+  return (
+    <HeaderStyle>
+      <Title>WIKI Page</Title>
+    </HeaderStyle>
+  );
 };
 
 export default Header;
+
+const HeaderStyle = styled.header`
+  height: 12vh;
+  display: flex;
+`;
+
+const Title = styled.div`
+  width: 100%;
+  padding: 20px;
+  font-size: 50px;
+  color: black;
+`;
