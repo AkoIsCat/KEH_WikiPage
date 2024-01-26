dc8cf51e3e959ab939b0c5ef0a3c76045bb05cfd
KimEunHye <gpqjr00@naver.com>
2024-01-27 04:31:15 +0900

    Feat: api 호출 및 추가/생성 관련 코드 App.tsx로 이동

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,55 @@
+import { useEffect, useState } from 'react';
 import { Route, Routes } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { DataProps } from 'types/types';
+import { getWikiData } from 'api/axios';
 
 import MainPage from 'pages/MainPage';
 import DetailPage from 'pages/DetailPage';
 import ModifyPage from 'pages/ModifyPage';
 
 function App() {
+  const [data, setData] = useState<DataProps[]>([]);
   const queryClient = new QueryClient();
+
+  useEffect(() => {
+    const getData = async () => {
+      const response = await getWikiData();
+      const data = await response?.data.data;
+      setData(data);
+    };
+
+    getData();
+  }, []);
+
+  const addPost = (addData: DataProps[]) => {
+    const newData: DataProps[] = [...data, ...addData];
+    setData(newData);
+  };
+
+  const modifyPost = (modifyData: DataProps[]) => {
+    const mdData = modifyData[0];
+    const newData = data.map((item) =>
+      item.id === mdData.id
+        ? {
+            ...item,
+            title: mdData.title,
+            contents: mdData.contents,
+          }
+        : item
+    );
+    setData(newData);
+  };
 
   return (
     <QueryClientProvider client={queryClient}>
       <Routes>
-        <Route path="/" element={<MainPage />} />
+        <Route path="/" element={<MainPage addPost={addPost} data={data} />} />
         <Route path="/:id" element={<DetailPage />} />
-        <Route path="/:id/modify" element={<ModifyPage />} />
+        <Route
+          path="/:id/modify"
+          element={<ModifyPage modifyPost={modifyPost} />}
+        />
       </Routes>
     </QueryClientProvider>
   );
