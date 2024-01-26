f5515d32d3adfeda746f4a479f6d156757eae7ae
KimEunHye <gpqjr00@naver.com>
2024-01-26 20:50:51 +0900

    Feat: 필요한 type 정의

--- src/types/types.ts
+++ src/types/types.ts
@@ -1,3 +1,5 @@
+import { ReactNode } from 'react';
+
 // 공통 타입
 export interface ListItemProps {
   id: number;
@@ -10,4 +12,29 @@ export interface PagingProps {
   pageChangeHandler: (page: number) => void;
   page: number;
   totalItemCount: number;
+}
+
+// children 컴포넌트 타입
+export interface ChildrenProps {
+  children: ReactNode;
+}
+
+// button 컴포넌트 타입
+export interface ButtonProps {
+  title: string;
+  onClick?: () => void;
+}
+
+// data 타입
+export interface DataProps {
+  id: number;
+  title: string;
+  contents: string;
+}
+
+// modal 컴포넌트 타입
+export interface ModalProps {
+  onClose: () => void;
+  onClickSubmit: (data: DataProps[]) => void;
+  idNumber: number;
 }
