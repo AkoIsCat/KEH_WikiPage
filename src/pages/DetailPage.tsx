13e744ebe9ac794f8af6b8d10f3f8f4e7dc42693
KimEunHye <gpqjr00@naver.com>
2024-01-27 00:43:06 +0900

    Feat: detail 페이지 세부 코드 작성

--- src/pages/DetailPage.tsx
+++ src/pages/DetailPage.tsx
@@ -1,18 +1,54 @@
-import { useLocation } from 'react-router-dom';
+import styled from 'styled-components';
+import { useLocation, useNavigate } from 'react-router-dom';
 
 import Background from 'components/UI/Background';
-import ContentWrap from 'components/UI/ContentsWrap';
+import ContentsWrap from 'components/UI/ContentsWrap';
+import Button from 'components/UI/Button';
 
 const DetailPage = () => {
   const { state } = useLocation();
+  const navigate = useNavigate();
   console.log(state);
+
+  const onClickList = () => {
+    navigate('/');
+  };
+
+  const onClickModify = () => {
+    navigate(`/${state.id}/modify`, { state: state });
+  };
+
   return (
     <Background>
-      <ContentWrap>
-        <div>{state.contents}</div>
-      </ContentWrap>
+      <ContentsWrap>
+        <Title>{state.title}</Title>
+        <Contents>{state.contents}</Contents>
+        <ButtonWrap>
+          <Button title="목록" onClick={onClickList} />
+          <Button title="수정" onClick={onClickModify} />
+        </ButtonWrap>
+      </ContentsWrap>
     </Background>
   );
 };
 
 export default DetailPage;
+
+const Title = styled.div`
+  font-size: 40px;
+  font-weight: bold;
+  margin: 10px 20px;
+  padding-bottom: 20px;
+  border-bottom: 3px solid #ccd0e7;
+`;
+
+const Contents = styled.div`
+  height: 300px;
+  margin: 20px;
+`;
+
+const ButtonWrap = styled.div`
+  display: flex;
+  justify-content: center;
+  margin: 0 20px;
+`;
