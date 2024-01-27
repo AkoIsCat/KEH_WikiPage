e8d331b55cdae5f9ea6d0732fc8c6b84c1a26bff
KimEunHye <gpqjr00@naver.com>
2024-01-27 21:23:23 +0900

    Feat: autoLinking 기능 적용 및 useLocation 제거 + state data를 받아와 렌더링 하도록 수정

--- src/pages/DetailPage.tsx
+++ src/pages/DetailPage.tsx
@@ -1,29 +1,50 @@
 import styled from 'styled-components';
-import { useLocation, useNavigate } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
 
 import Background from 'components/UI/Background';
 import ContentsWrap from 'components/UI/ContentsWrap';
 import Button from 'components/UI/Button';
 import Header from 'components/Header/Header';
+import AutoLinkingContent from 'utils/AutoLinkingContent';
+import { DetailProps, DataProps } from 'types/types';
+import LoadingSpinner from 'components/UI/LoadingSpinner';
 
-const DetailPage = () => {
-  const { state } = useLocation();
+const DetailPage = ({ data }: DetailProps) => {
+  const { id } = useParams();
   const navigate = useNavigate();
+
+  if (!id || !data) {
+    return <LoadingSpinner />;
+  }
+
+  const currentData = data[+id - 1];
 
   const onClickList = () => {
     navigate('/');
   };
 
+  const titleArray = data.map((item: DataProps) => item.title);
+
   const onClickModify = () => {
-    navigate(`/${state.id}/modify`, { state: state });
+    navigate(`/${currentData.id}/modify`, { state: currentData });
   };
+
+  if (!currentData) {
+    return <LoadingSpinner />;
+  }
 
   return (
     <Background>
       <ContentsWrap>
         <Header />
-        <Title>{state.title}</Title>
-        <Contents>{state.contents}</Contents>
+        <Title>{currentData.title}</Title>
+        <Contents>
+          {AutoLinkingContent({
+            contents: currentData.contents,
+            titles: titleArray,
+            allData: data,
+          })}
+        </Contents>
         <ButtonWrap>
           <Button title="목록" onClick={onClickList} type="button" />
           <Button title="수정" onClick={onClickModify} type="submit" />
