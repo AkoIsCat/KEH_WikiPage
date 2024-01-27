90e8a68e53246f187bf5053c9d3484a2c9789545
KimEunHye <gpqjr00@naver.com>
2024-01-27 21:22:20 +0900

    Feat: 콘텐츠 내용의 autoLinking 기능 컴포넌트 생성

---
+++ src/utils/AutoLinkingContent.tsx
@@ -0,0 +1,49 @@
+import { ReactNode } from 'react';
+import { AutoLinkingProps } from 'types/types';
+import { Link } from 'react-router-dom';
+
+const AutoLinkingContent = ({
+  contents,
+  titles,
+  allData,
+}: AutoLinkingProps) => {
+  let currentIndex = 0;
+  const elements: ReactNode[] = [];
+
+  // contents에서 title에 속하는 부분을 찾아서 렌더링
+  for (const title of titles) {
+    const indexOfTitle = contents.indexOf(title, currentIndex);
+
+    if (indexOfTitle !== -1) {
+      // title 이전의 부분을 p 태그로 렌더링
+      if (currentIndex < indexOfTitle) {
+        const textBeforeTitle = contents.slice(currentIndex, indexOfTitle);
+        elements.push(<span key={textBeforeTitle}>{textBeforeTitle}</span>);
+      }
+
+      const findId = allData.filter(
+        (item) => item.title === title && item.id
+      )[0];
+
+      // title을 Link 컴포넌트로 렌더링
+      elements.push(
+        <Link to={`/${findId.id}`} key={title}>
+          {title}
+        </Link>
+      );
+
+      // 다음 탐색을 위해 currentIndex 갱신
+      currentIndex = indexOfTitle + title.length;
+    }
+  }
+
+  // title 이후의 부분을 p 태그로 렌더링
+  if (currentIndex < contents.length) {
+    const textAfterTitles = contents.slice(currentIndex);
+    elements.push(<span key={textAfterTitles}>{textAfterTitles}</span>);
+  }
+
+  return elements;
+};
+
+export default AutoLinkingContent;
